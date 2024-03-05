d5009418e99eed0e66560b77ff04a6de9937ac2e
Vorleak Yek <vorleakyek@gmail.com>
2024-03-04 21:54:01 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
